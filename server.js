7439f883ce239a558daf02a780c1a04517907abd
Joelle DesChatelets <jdeschat@gmail.com>
2021-09-05 17:33:24 -0400

    current code

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const dbJson = require('./db/db.json')
+var uuidv1 = require('uuidv1')
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -27,6 +28,7 @@ app.post('/api/notes', (req, res) => {
     const dataNotes = fs.readFileSync(path.join(__dirname, './db/db.json'), "utf-8");
     const parseNotes = JSON.parse(dataNotes);
     parseNotes.push(req.body);
+    // id: uuidv1();
 
     fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify(parseNotes), "utf-8");
     res.json("You have successfully added a note!");
@@ -36,15 +38,8 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
-// app.delete("/api/notes/:id", (req, res) => {
-//     const dataNotes = fs.readFileSync(path.join(__dirname, './db/db.json'), "utf-8");
-//     const parseNotes = JSON.parse(dataNotes);
-
-//     parseNotes.remove(req.params.id);
-// });
-
 app.delete("/api/notes/:id", function (req, res) {
-
+    console.log(uuidv1())
     console.log("Req.params:", req.params);
     let deletedNote = parseInt(req.params.id);
     console.log(deletedNote);
@@ -52,11 +47,9 @@ app.delete("/api/notes/:id", function (req, res) {
 
     for (let i = 0; i < dbJson.length; i++) {
         if (deletedNote === dbJson[i].id) {
-            // delete noteContents[i];
             dbJson.splice(i, 1);
 
             let noteJson = JSON.stringify(dbJson, null, 2);
-            //using placeholders from .json file
             writeFileAsync("./db/db.json", noteJson).then(function () {
                 console.log("Your note has been deleted!");
             });
